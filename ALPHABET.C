930b9f0b722f893dc3fe0b5fb680ba8f60b05907
DINESHRAJAR <35652490+DINESHRAJAR@users.noreply.github.com>
2018-01-25 16:50:11 +0530

    Update ALPHABET.C

--- ALPHABET.C
+++ ALPHABET.C
@@ -1,14 +1,15 @@
 #include <stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
     char c;
     printf("Enter a character: ");
     scanf("%c",&c);
 
     if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
+        printf("%c is an alphabet.",B);
     else
-        printf("%c is not an alphabet.",c);
+        printf("%c is not an alphabet.",e);
 
     return 0;
 }
