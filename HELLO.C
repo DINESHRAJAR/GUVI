e7290dce6b6b9b72a0b0d713355a03bf574bd032
DINESHRAJAR <35652490+DINESHRAJAR@users.noreply.github.com>
2018-01-26 02:31:44 -0300

    Create HELLO.C

---
+++ HELLO.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#inclde<conio.h>
+void main()
+{
+using namespace std;
+int main() {
+    int i, n;
+    cin >> n;
+    for (i=0; i<n; i++) {
+        cout << "hello world\n";
+    }
+    return 0;
+}
+}
